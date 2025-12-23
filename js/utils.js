8a7fa8463350100a1b69daa3f972e5c0611e07c9
MSI\Linda <linda@agilebytestudio.com>
2025-12-23 12:05:49 +0800

    less polution in log

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 // Version string (update timestamp and version number on each code change)
 // Format: YYYY-MM-DD-HH:MM-vX.X
-export const VERSION = '2025-12-18-v001';
+export const VERSION = '2025-12-23-v01';
 
 // Enums and pools
 export const VisitState = Object.freeze({
